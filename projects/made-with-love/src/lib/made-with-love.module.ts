0beafd7a39e6fb8c4a550fef3df01c5347250c3b
David Lawson <david.lawson@canstar.com.au>
2018-12-17 17:03:28 +1000

    Added a test view for buttons

--- projects/made-with-love/src/lib/made-with-love.module.ts
+++ projects/made-with-love/src/lib/made-with-love.module.ts
@@ -4,20 +4,16 @@ import { CommonModule } from '@angular/common';
 import { createCustomElement } from '@angular/elements';
 
 import { MadeWithLoveComponent } from './components/made-with-love/made-with-love.component';
-import { TestComponent } from './components/test/test.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [MadeWithLoveComponent, TestComponent],
-  entryComponents: [MadeWithLoveComponent, TestComponent]
+  declarations: [MadeWithLoveComponent],
+  entryComponents: [MadeWithLoveComponent]
 })
 export class MadeWithLoveModule {
   constructor(private injector: Injector) {
     const madeWithLoveElement = createCustomElement(MadeWithLoveComponent, { injector });
     customElements.define('made-with-love', madeWithLoveElement);
-
-    const testElement = createCustomElement(TestComponent, { injector });
-    customElements.define('test', testElement);
   }
 
   ngDoBootstrap() {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'web-components-lib-poc';
-  data = ['Item left', 'Item middle', 'Item right'];
 }
