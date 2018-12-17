3ece677321863ea5f7e0f93bf5cf73a5e8719391
David Lawson <david.lawson@canstar.com.au>
2018-12-17 14:44:10 +1000

    Added  a buttons library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
-import { MadeWithLoveModule } from 'made-with-love';
-import { BannerTestModule } from 'banner-test';
+import { MadeWithLoveModule } from '../../projects/made-with-love/src/public_api';
+import { BannerTestModule } from '../../projects/banner-test/src/public_api';
+import { ButtonsModule } from '../../projects/buttons/src/public_api';
+
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [ BrowserModule, MadeWithLoveModule, BannerTestModule],
+  imports: [BrowserModule, MadeWithLoveModule, BannerTestModule, ButtonsModule],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
