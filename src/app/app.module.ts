ec04b912d5fdf02a9a7832f81b7cc48437134453
Christopher Brewin <chris.brewin@canstar.com.au>
2018-12-17 12:56:10 +1000

    removed banner-test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { MadeWithLoveModule } from 'made-with-love';
-import { BannerTestModule } from 'banner-test';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [ BrowserModule, MadeWithLoveModule, BannerTestModule],
+  imports: [ BrowserModule, MadeWithLoveModule ],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
