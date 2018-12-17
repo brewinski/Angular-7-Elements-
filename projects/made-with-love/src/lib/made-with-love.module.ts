16c204734614461deefb40f96f0ba227b47eb71b
Christopher Brewin <chris.brewin@canstar.com.au>
2018-12-17 15:37:25 +1000

    Implemented example components

--- projects/made-with-love/src/lib/made-with-love.module.ts
+++ projects/made-with-love/src/lib/made-with-love.module.ts
@@ -3,17 +3,21 @@ import { NgModule, Injector } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { createCustomElement } from '@angular/elements';
 
-import { MadeWithLoveComponent } from './made-with-love.component';
+import { MadeWithLoveComponent } from './components/made-with-love/made-with-love.component';
+import { TestComponent } from './components/test/test.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [MadeWithLoveComponent],
-  entryComponents: [MadeWithLoveComponent]
+  declarations: [MadeWithLoveComponent, TestComponent],
+  entryComponents: [MadeWithLoveComponent, TestComponent]
 })
 export class MadeWithLoveModule {
   constructor(private injector: Injector) {
     const madeWithLoveElement = createCustomElement(MadeWithLoveComponent, { injector });
     customElements.define('made-with-love', madeWithLoveElement);
+
+    const testElement = createCustomElement(TestComponent, { injector });
+    customElements.define('test', testElement);
   }
 
   ngDoBootstrap() {}
