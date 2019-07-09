84f22045fe9344d75b3794ea929fee994a529fcf
Chris Brewin <chris.brewin@canstar.com.au>
2019-07-09 11:53:46 +1000

    changes expersion to named function

--- projects/form/src/lib/constants.ts
+++ projects/form/src/lib/constants.ts
@@ -2,14 +2,10 @@ import { Provider, forwardRef } from '@angular/core';
 import { NG_VALUE_ACCESSOR } from '@angular/forms';
 
 
-const getAccessValidatorProvider = (Component: Object): Provider => {
+export function getAccessValidatorProvider(Component: Object): Provider {
   return {
     provide: NG_VALUE_ACCESSOR,
     useExisting:  forwardRef(() => Component),
     multi: true
   };
-};
-
-export {
-  getAccessValidatorProvider
-};
+}
